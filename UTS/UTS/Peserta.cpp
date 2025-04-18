71a5e27418619e22322f49f7455e86ce7f5ae238
Robin <165838816+robinbro71@users.noreply.github.com>
2025-04-19 05:11:13 +0700

    Add files via upload

--- UTS/UTS/Peserta.cpp
+++ UTS/UTS/Peserta.cpp
@@ -1,5 +1,31 @@
 #include "Peserta.h"
+#include <fstream>
 
 void Peserta::tambahPeserta(){
+	system("cls");
+	string nama;
+	string institusi;
+	string email;
 
+	cin.ignore();
+
+	printf("Nama: ");
+	getline(cin, nama);
+	printf("\n");
+	printf("Institusi: ");
+	getline(cin, institusi);
+	printf("\n");
+	printf("Email: ");
+	getline(cin, email);
+	printf("\n");
+
+	fstream myFile;
+	myFile.open("peserta.txt", ios::app);  //write out = mengganti || app = menambahkan
+	if (myFile.is_open()) {
+		myFile << "Nama: " << nama << endl;
+		myFile << "Institusi: " << institusi << endl;
+		myFile << "Email: " << email << endl;
+		myFile << "---"<< endl;
+		myFile.close();
+	}
 }
